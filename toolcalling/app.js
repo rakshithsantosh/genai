79f661feb0a67d080f78ebaf458b4e7ca2e6c52b
rakshith_s <rakshithsantosh@gmail.com>
2025-08-19 19:06:54 +0530

    integrating tavily with tool calling

--- toolcalling/app.js
+++ toolcalling/app.js
@@ -1,67 +1,86 @@
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
 import dotenv from "dotenv";
 dotenv.config();
+
+const tvly = tavily({
+  apiKey: process.env.TAVILY_API_KEY,
+});
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 });
 
 async function main() {
-  const completion = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: [
-      {
-        role: "system",
-        content: `you are a smart assistant that can answer questions.
+  const messages = [
+    {
+      role: "system",
+      content: `you are a smart assistant that can answer questions.
         you have access to following tools:
         1. webSearch({query}):{query:string} //the search query to perform search on`,
-      },
-      { role: "user", content: "when is iphone 16 launched" },
-    ],
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description: "searches the web for the given query",
-          parameters: {
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "the search query to perform search on",
+    },
+    { role: "user", content: "when is iphone 16 launched" },
+  ];
+
+  while (true) {
+    const completion = await groq.chat.completions.create({
+      model: "llama-3.3-70b-versatile",
+      temperature: 0,
+      messages: messages,
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description: "searches the web for the given query",
+            parameters: {
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description: "when is iphone 16 launched",
+                },
               },
+              required: ["query"],
             },
-            required: ["query"],
           },
         },
-      },
-    ],
-    tool_choice: "auto",
-  });
+      ],
+      tool_choice: "auto",
+    });
 
-  const toolCalls = completion.choices[0].message.tool_calls;
-  console.log("Tool Calls:", toolCalls);
+    messages.push(completion.choices[0].message);
 
-  if (!toolCalls) {
-    console.log(`Assitant: ${completion.choices[0].message.content}`);
-    return;
-  }
+    const toolCalls = completion.choices[0].message.tool_calls;
+    //console.log("Tool Calls:", toolCalls);
 
-  for (const tool of toolCalls) {
-    console.log(`Tool: ${tool}`);
-    const functionName = tool.function.name;
-    const functionArgs = tool.function.arguments;
-
-    if (functionName === "webSearch") {
-      const toolResult = await webSearch(JSON.parse(functionArgs));
-      console.log(`Tool Result: ${toolResult}`);
-      return;
+    if (!toolCalls) {
+      console.log(`Assitant: ${completion.choices[0].message.content}`);
+      break;
     }
-  }
 
-  console.log(JSON.stringify(completion.choices[0].message, null, 2));
+    for (const tool of toolCalls) {
+      //console.log(`Tool: ${tool}`);
+      const functionName = tool.function.name;
+      const functionArgs = tool.function.arguments;
+
+      if (functionName === "webSearch") {
+        const toolResult = await webSearch(JSON.parse(functionArgs));
+
+        messages.push({
+          tool_call_id: tool.id,
+          role: "tool",
+          name: "webSearch",
+          content: toolResult,
+        });
+
+        //console.log(`Tool Result: ${toolResult}`);
+        return;
+      }
+    }
+
+    console.log(JSON.stringify(completion.choices[0].message, null, 2));
+  }
 }
 
 main();
@@ -70,5 +89,11 @@ async function webSearch({ query }) {
   //tavily api call
   console.log("calling websearch");
 
-  return "iphone 16 was launched on 20 september 2024";
+  const response = await tvly.search(query, { maxResults: 1 });
+
+  const finalResult = response.results
+    .map((result) => result.content)
+    .join("\n\n");
+
+  return finalResult;
 }
